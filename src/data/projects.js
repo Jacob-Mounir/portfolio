5d248008e7b8293d556536e8379ee5c05a0c8b5f
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-11-10 10:53:42 +0100

    feat: initial portfolio commit

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+	{
+		image: '/public/images/oasis.png',
+		title: 'Oasis ',
+		lang: 'React, CSS, UI/UX',
+		description: 'Group Project for a fine dining restaurant',
+		link: 'https://jacob-mounir.github.io/the-oasis/'
+	},
+
+	{
+		image: '/public/images/masafejuja.png',
+		title: 'Fine Dining Restaurant',
+		lang: 'React, CSS, UI/UX',
+		description: 'School project for a fine dining restaurant',
+		link: 'https://ha-fed23-js-2.github.io/ruby-bananas/'
+	},
+
+	{
+		image: '/public/images/clouds.png',
+		title: 'Clouds',
+		lang: 'UI/UX, Figma',
+		description: 'Short info about',
+		link: ''
+	},
+
+	{
+		image: '/public/images/pokemon.jpg',
+		title: 'Pokémon Team Manager',
+		lang: 'HTML, CSS & JavaScript',
+		description: 'Short info about',
+		link: 'https://jacob-mounir.github.io/pokemon/'
+
+	}
+]
+
+export default projects;
---
+++ src/types/vanilla-tilt.d.ts
@@ -0,0 +1,9 @@
+declare const VanillaTilt: {
+  init(elements: NodeListOf<Element> | Element[], options?: {
+    max?: number;
+    speed?: number;
+    glare?: boolean;
+    'max-glare'?: number;
+    scale?: number;
+  }): void;
+};
