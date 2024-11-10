c2918a5034b226dbe963fc93f7ca0b241fcf5da9
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-11-10 18:21:05 +0100

    rm favicon from code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/your-repo-name',
+  base: '/portfolio',
   integrations: [tailwind(), react()],
   output: 'static'
 });
