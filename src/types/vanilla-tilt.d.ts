c736eb032159376769695855b021e3867206761b
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-11-10 11:23:16 +0100

    deploy: latest build

--- src/types/vanilla-tilt.d.ts
+++
@@ -1,9 +0,0 @@
-declare const VanillaTilt: {
-  init(elements: NodeListOf<Element> | Element[], options?: {
-    max?: number;
-    speed?: number;
-    glare?: boolean;
-    'max-glare'?: number;
-    scale?: number;
-  }): void;
-};
